2bd471fea9bf299e21ff306a23f4c226299ab4a8
Luke Karrys <lukekarrys@gmail.com>
2018-05-11 16:24:42 -0700

    no ga

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const themePath = 'node_modules/jsonresume-theme-stackoverflow'
 const theme = fs.readFileSync(`${themePath}/index.js`, 'utf-8')
 const compileCss = `require('sync-exec')('./node_modules/.bin/lessc style.less').stdout`
 
-const analytics = "<script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');ga('create', 'UA-8402584-23', 'auto');ga('send', 'pageview');</script>"
-
 // The theme will be run in the context of project directory, which requires
 // the template path to point back to the installed theme module and the patched
 // theme to be compiled manually
@@ -14,7 +12,7 @@ const patchedTheme = theme
   .replace(/(var tpl = )(.*?)(;)/, `
     $1$2
       .replace(/opacity:[10]/g, '')
-      .replace(/<script>[\\s\\S]*<\\/script>/g, "${analytics}")
+      .replace(/<script>[\\s\\S]*<\\/script>/g, "")
       .replace(/item display none/g, 'item display')
       .replace(/fa-caret-down/g, 'fa-caret-right')
   `)
