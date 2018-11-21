3662cf77fd3ff749630043c4de3197386c533e8b
Luke Karrys <lukekarrys@gmail.com>
2018-11-20 17:23:53 -0700

    merge

--- resume.js
+++ resume.js
@@ -1,1 +1,20 @@
-module.exports = require("./resumes/default.json");
+const fs = require("fs");
+const _ = require("lodash");
+
+const resumes = [
+  "default",
+  ...(process.env.RESUMES || "").split(",").filter(Boolean)
+].map(r => require(`./resumes/${r}.json`));
+
+fs.writeFileSync(
+  "./resume.json",
+  JSON.stringify(
+    _.mergeWith(...resumes, (objValue, srcValue) => {
+      if (_.isArray(objValue)) {
+        return objValue.concat(srcValue);
+      }
+    }),
+    null,
+    2
+  )
+);
