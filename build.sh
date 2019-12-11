79e47578e690c99640eb6d9af67404928601afc9
Luke Karrys <lukekarrys@gmail.com>
2019-12-10 17:44:29 -0700

    debug build

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+NODE_ENV=production npm start -- --silent --port 9000 & PID=$!
+echo "Server is $PID"
+
+sleep 2
+
+echo "Requesting localhost site to save html to build"
+curl -s http://localhost:9000 > /dev/null
+
+if test -f "build/index.html"; then
+  echo "Built file exists"
+else
+  echo "Built file DOES NOT exist"
+fi
+
+echo "Kill $PID"
+kill -INT $PID
