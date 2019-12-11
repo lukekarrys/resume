c402f7bbccc661a3003742aee15eed3541c28a1c
Luke Karrys <lukekarrys@gmail.com>
2019-12-10 20:38:24 -0700

    build sh

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+./node_modules/.bin/rimraf build
+node resume.js
 NODE_ENV=production ./node_modules/.bin/resume serve --theme stackoverflow --dir build/ --silent --port 9000 & PID=$!
 echo "Server is $PID"
 
@@ -16,3 +18,8 @@ fi
 
 echo "Kill $PID"
 kill -INT $PID
+
+node build.js
+./node_modules/.bin/cpr build/index.html build/404.html
+./node_modules/.bin/cpr assets build
+echo "/*   /404.html   404" > build/_redirects
